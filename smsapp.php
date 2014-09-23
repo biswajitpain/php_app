2ef4f65532b533e27eba48b7ad52bfd31273ba64
linux <linux@LINUX_VM.latam.americas.gcn.local>
2014-09-23 05:22:10 -0400

    v1

---
+++ smsapp.php
@@ -0,0 +1,13 @@
+<?php
+if(isset($_POST['submit'])) 
+{ 
+    $name = $_POST['name'];
+    echo $_SERVER['PHP_SELF'];
+    echo "User Has submitted the form and entered this name : <b> $name </b>";
+    echo "<br>You can use the following form again to enter a new name."; 
+}
+?>
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+   <input type="text" name="name"><br>
+   <input type="submit" name="submit" value="Submit Form"><br>
+</form>
---
+++ test.php
@@ -0,0 +1,4 @@
+<?php
+echo "Hello Biswajit";
+echo"\n";
+?>
